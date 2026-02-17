2039e6b406121b510440da1d2c6bdf9d6b6e223a
Rachana Kotagiri <kotagirirachana99@gmail.com>
2026-02-17 09:31:08 +0000

    daily weather model created

---
+++ macros/data_utils.sql
@@ -0,0 +1,29 @@
+{% macro function1(x) %}
+
+CASE WHEN TO_TIMESTAMP({{x}})< CURRENT_DATE THEN 'PAST'
+ELSE 'FUTURE' END 
+
+{% endmacro %}
+
+{% macro getseason(x)%}
+
+CASE WHEN MONTH(TO_TIMESTAMP({{x}})) in (12,1,2)
+    THEN 'WINTER'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) in (3,4,5)
+    THEN 'SPRING'
+    WHEN MONTH(TO_TIMESTAMP({{x}})) in (6,7,8)
+    THEN 'SUMMER'
+    ELSE 'AUTUMN' 
+    END
+
+{% endmacro %}
+
+{% macro daytype(x)%}
+
+CASE 
+WHEN DAYNAME(TO_TIMESTAMP({{x}})) in ('Sat','Sun')
+THEN 'WEEKEND'
+ELSE 'BUSINESSDAY'
+END
+
+{% endmacro %}
